f0c10c2cb78ac30a3fd85611fc3f19b235bf8415
fasihrem <fasihrem@gmail.com>
2024-09-16 23:28:45 +0500

    basic edits in top.py

--- Backend/top.py
+++ Backend/top.py
@@ -111,7 +111,7 @@ for subreddits in subReddits:
         count = count+1
         print("records: ", count)
 
-    df2.to_csv("./data/macbook/"+subreddits+"_top.csv", sep=',', encoding="utf-8")
+    df2.to_csv("./data/uni_pc/"+subreddits+"_top.csv", sep=',', encoding="utf-8")
     df2 = pd.DataFrame(columns=df2.columns)
     count = 0
 
